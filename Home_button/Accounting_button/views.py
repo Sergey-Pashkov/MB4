1023dbf6755631671cfaa35466076f388d0a4fff
Sergey-Pashkov <Pashkov-sa@ya.ru>
2024-06-19 09:57:25 +0700

    Добавлено приветствие на дашборд пользователя

--- Home_button/Accounting_button/views.py
+++ Home_button/Accounting_button/views.py
@@ -1,3 +1,4 @@
+'''''
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login
@@ -41,3 +42,44 @@ def owner_dashboard(request):
 
 
 # Create your views here.
+
+'''
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import CustomUser
+from .forms import CustomUserCreationForm
+
+@login_required
+def dashboard(request):
+    if request.user.user_type == 'accountant':
+        return redirect('accountant_dashboard')
+    elif request.user.user_type == 'director':
+        return redirect('director_dashboard')
+    elif request.user.user_type == 'owner':
+        return redirect('owner_dashboard')
+
+@login_required
+def accountant_dashboard(request):
+    return render(request, 'Accounting_button/accountant_dashboard.html', {'user': request.user})
+
+@login_required
+def director_dashboard(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('director_dashboard')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'Accounting_button/director_dashboard.html', {'form': form, 'user': request.user})
+
+@login_required
+def owner_dashboard(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('owner_dashboard')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'Accounting_button/owner_dashboard.html', {'form': form, 'user': request.user})
