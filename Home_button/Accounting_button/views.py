f4255b356e9a30efbe38ff02e68d18cccd26107a
Sergey-Pashkov <Pashkov-sa@ya.ru>
2024-06-24 14:16:20 +0700

    Удален лишний код с дашборда собственника. Справочники: Константы, Клиенты, Пользователи добавлены на дашборд собственника

--- Home_button/Accounting_button/views.py
+++ Home_button/Accounting_button/views.py
@@ -29,32 +29,13 @@ def director_dashboard(request):
     return render(request, 'Accounting_button/director_dashboard.html', {'user': request.user})
 
 # Дашборд собственника с обработкой формы создания пользователя и редактирования констант
+# views.py
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+
 @login_required
 def owner_dashboard(request):
-    if request.method == 'POST' and 'create_user' in request.POST:
-        form_user = CustomUserCreationForm(request.POST)
-        if form_user.is_valid():
-            form_user.save()
-            return redirect('owner_dashboard')
-    else:
-        form_user = CustomUserCreationForm()
-
-    if request.method == 'POST' and 'constant_id' in request.POST:
-        constant_id = request.POST.get('constant_id')
-        constant = Constant.objects.get(id=constant_id)
-        form_constant = ConstantForm(request.POST, instance=constant)
-        if form_constant.is_valid():
-            form_constant.save()
-            return redirect('owner_dashboard')
-    else:
-        constants = Constant.objects.all()
-        constants_forms = [(constant, ConstantForm(instance=constant)) for constant in constants]
-
-    return render(request, 'Accounting_button/owner_dashboard.html', {
-        'form_user': form_user,
-        'constants_forms': constants_forms,
-        'user': request.user,
-    })
+    return render(request, 'Accounting_button/owner_dashboard.html', {'user': request.user})
 
 
 @login_required
